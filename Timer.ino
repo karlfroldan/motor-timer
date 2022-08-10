dd44642891fd9e28b719bb1558225294a66be4f0
Karl Frederick Roldan <karlfroldan@gmail.com>
2022-08-11 02:11:38 +0800

    finalize for LEDs

--- Timer.ino
+++ Timer.ino
@@ -6,7 +6,12 @@
  * at least. 
  */
 
-#define MOTOR_1_PIN 10
+#define MOTOR_0_PIN 10
+#define MOTOR_1_PIN 8
+#define MOTOR_2_PIN 7
+
+// Comment this out to turn off all SERIAL printlns.
+// #define DEBUG
 
 using ulong = unsigned long;
 
@@ -28,13 +33,13 @@ class Motor {
   public:
     Motor(int pin, 
       ulong t0,     // Switch time
-      ulong t1_on,  // How many milliseconds will t1 be turned on?
-      ulong t1_off, // How many milliseconds will t1 be turned off?
-      ulong t2_on,  // How many milliseconds will t2 be turned on?
-      ulong t2_off  // How many milliseconds will t2 be turned off?
+      ulong t1_on,  // How many seconds will t1 be turned on?
+      ulong t1_off, // How many seconds will t1 be turned off?
+      ulong t2_on,  // How many seconds will t2 be turned on?
+      ulong t2_off  // How many seconds will t2 be turned off?
     ) : t_switch {t0}, 
-        t1 {ms_to_s(t1_on), ms_to_s(t1_off)},
-        t2 {ms_to_s(t2_on), ms_to_s(t2_off)},
+        t1 {t1_on, t1_off, pin},
+        t2 {t2_on, t2_off, pin},
         last_update {0}
     {
       pinMode(pin, OUTPUT);
@@ -47,34 +52,69 @@ class Motor {
     void update(ulong time) {
       // Poll t_switch
       if (time % t_switch == 0 && last_update != time) {
-        is_t1 = !is_t1;
+        is_t1 = !is_t1; // switch.
         last_update = time;
-        Serial.println(time);
+        #ifdef DEBUG
+        Serial.print("Updating t0: ");
+        Serial.print(time);
+        Serial.print(" seconds. Timer is now: ");
+
+        if (is_t1) {
+          Serial.println("t1");
+        } else {
+          Serial.println("t2");
+        }
+        #endif
       }
 
       // Timer::update has its own `last_update`.
       if (is_t1) {
         t1.update(time);
       } else {
-        t1.update(time);
+        t2.update(time);
       }
     }
   private:
     /* The timer is a struct that contains information about one of the three timers. */
-    struct Timer {
+    struct Timer {        // I'm just checking for struct padding.
       ulong time_on;      // 32 bits
       ulong time_off;     // 32 bits
       ulong last_update;  // 32 bits
-      bool is_on; // A flag // padded so 32 bits.
-      Timer(ulong on, ulong off) : time_on{on}, time_off {off}, is_on {false} {}
+      int pin;            // 16 bits
+      bool is_on;         // A flag
+      Timer(ulong on, ulong off, int _pin) 
+        : time_on{on}, time_off {off}, is_on {false}, pin {_pin}, last_update {0} {}
 
       void update(ulong time) {
         if (is_on) {
           // Check how many seconds should this be turned on.
-          
+          if (time % time_on == 0 && time != last_update) {
+            // Then we should switch.
+            is_on = false;
+            last_update = time;
+            digitalWrite(pin, LOW);
+            #ifdef DEBUG
+            Serial.print("Switched to off at time ");
+            Serial.print(time);
+            Serial.println(" seconds");
+            #endif
+          }
         } else {
-          
+          if (time % time_off == 0 && time != last_update) {
+            is_on = true; // switch.
+            last_update = time;
+            digitalWrite(pin, HIGH);
+            #ifdef DEBUG
+            Serial.print("Switched to on at time ");
+            Serial.print(time);
+            Serial.println(" seconds");
+            #endif
+          }
         }
+      }
+
+      void reset() {
+        last_update = 0;
       }
     };
     
@@ -90,20 +130,40 @@ class Motor {
     
 };
 
-Motor m0 = m0 = Motor(MOTOR_1_PIN, 
-    s_to_ms(10), // Switch every 10 seconds
-    s_to_ms(3),  // t1 is on for 3 seconds
-    s_to_ms(2),  // t1 is on for 2 seconds
-    s_to_ms(4),  // t2 is on for 4 seconds
-    s_to_ms(1)  // t2 is off for 1 second.
+Motor m0 = m0 = Motor(MOTOR_0_PIN, 
+    15, // Switch every 15 seconds
+    5,  // t1 is on for 5 seconds
+    3,  // t1 is on for 3 seconds
+    1,  // t2 is on for 1 second
+    1   // t2 is off for 1 second.
+);
+
+Motor m1 = Motor(MOTOR_1_PIN, 
+    30, // Switch every 10 seconds
+    15, // t1 is on for 15 seconds
+    2,  // t1 is on for 2 seconds
+    4,  // t2 is on for 4 seconds
+    8   // t2 is off for 8 seconds.
+);
+
+Motor m2 = Motor(MOTOR_2_PIN, 
+    24, // Switch every 24 seconds
+    3,  // t1 is on for 3 seconds
+    7,  // t1 is on for 7 seconds
+    9,  // t2 is on for 9 seconds
+    2   // t2 is off for 2 seconds.
 );
 
 void setup() {
+  #ifdef DEBUG
   Serial.begin(230400);
+  #endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  ulong time { ms_to_s(micros()) };
+  ulong time { ms_to_s(millis()) };
   m0.update(time);
+  m1.update(time);
+  m2.update(time);
 }
